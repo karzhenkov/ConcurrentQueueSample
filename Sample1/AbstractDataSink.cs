4ab1799c5c12e65313ff3d3d326b11b2e8efa52c
Alexander Karzhenkov <karzhenkov@mail.ru>
2018-04-10 19:58:57 +0500

    Класс AbstractDataSink и его тестовая реализация

---
+++ Sample1/AbstractDataSink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public abstract class AbstractDataSink<Item>
+    {
+        private ConcurrentQueue<Item> _queue = new ConcurrentQueue<Item>();
+        private int _count;
+
+        protected abstract Task ProcessItem(Item item);
+
+        public void Enqueue(Item item)
+        {
+            _queue.Enqueue(item);
+            if (Interlocked.Increment(ref _count) == 1) ProcessQueue();
+        }
+
+        private async void ProcessQueue()
+        {
+            do
+            {
+                Item item;
+                _queue.TryDequeue(out item);
+                await ProcessItem(item);
+            } while (Interlocked.Decrement(ref _count) != 0);
+        }
+    }
+}
---
+++ Sample1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    class DataSink : AbstractDataSink<string>
+    {
+        protected override async Task ProcessItem(string item)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(150));
+            await Console.Out.WriteLineAsync(item);
+        }
+    }
+
+    class Program
+    {
+        static async void GenerateItemsAsync(int id, DataSink sink)
+        {
+            var random = new Random(id);
+
+            while (true)
+            {
+                int number = random.Next(1000);
+                await Task.Delay(TimeSpan.FromMilliseconds(number));
+                sink.Enqueue($"{id}: {number}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var sink = new DataSink();
+            for (int i = 0; i < 5; i++) GenerateItemsAsync(i, sink);
+            Console.ReadLine();
+        }
+    }
+}
