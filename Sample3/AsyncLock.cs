05d8a122b1f90dd3aad08ce654b45a6fffd746cb
Alexander Karzhenkov <karzhenkov@mail.ru>
2018-04-24 19:03:59 +0500

    Добавлена реализация класса AcquireLock

--- Sample3/AsyncLock.cs
+++ Sample3/AsyncLock.cs
@@ -1,16 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sample
 {
     class AsyncLock
     {
-        public Task<IDisposable> AcquireAsync()
+        private Task _task = Task.CompletedTask;
+
+        private class Pretender : IDisposable
         {
-            throw new NotImplementedException();
+            private TaskCompletionSource<object> _tcs = new TaskCompletionSource<object>();
+
+            public Task Task => _tcs.Task;
+
+            public void Dispose()
+            {
+                _tcs.SetResult(null);
+            }
+        }
+
+        public async Task<IDisposable> AcquireAsync()
+        {
+            var pretender = new Pretender();
+            var task = Interlocked.Exchange(ref _task, pretender.Task);
+            
+            if (!task.IsCompleted)
+            {
+                await task;
+                await Task.Yield();
+            }
+
+            return pretender;
         }
     }
 }
