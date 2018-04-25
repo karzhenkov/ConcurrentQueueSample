0f4489f04a0482ef1441b846686e7c3f2016d04d
Alexander Karzhenkov <karzhenkov@mail.ru>
2018-04-25 19:04:20 +0500

    AsyncLock: исключены избыточные вызовы "await Task.Yield()"

--- Sample3/AsyncLock.cs
+++ Sample3/AsyncLock.cs
@@ -6,31 +6,52 @@ namespace Sample
 {
     class AsyncLock
     {
-        private Task _task = Task.CompletedTask;
+        private Pretender _last = Pretender.CreateCompleted();
 
         private class Pretender : IDisposable
         {
-            private TaskCompletionSource<object> _tcs = new TaskCompletionSource<object>();
+            private TaskCompletionSource<object> _tcs;
+            private Task _task;
+            private bool _completed;
 
-            public Task Task => _tcs.Task;
+            private Pretender() { }
+
+            public static Pretender CreateCompleted()
+            {
+                return new Pretender { _task = Task.CompletedTask, _completed = true };
+            }
+
+            public static Pretender Create()
+            {
+                var tcs = new TaskCompletionSource<object>(null);
+                return new Pretender { _tcs = tcs, _task = tcs.Task };
+            }
+
+            public async Task WaitAsync()
+            {
+                await _task;
+                lock (this) if (_completed) return;
+                await Task.Yield();
+            }
 
             public void Dispose()
             {
-                _tcs.SetResult(null);
+                if (_tcs == null) return;
+
+                lock (this)
+                {
+                    _tcs.SetResult(null);
+                    _completed = true;
+                }
+
+                _tcs = null;
             }
         }
 
         public async Task<IDisposable> AcquireAsync()
         {
-            var pretender = new Pretender();
-            var task = Interlocked.Exchange(ref _task, pretender.Task);
-            
-            if (!task.IsCompleted)
-            {
-                await task;
-                await Task.Yield();
-            }
-
+            var pretender = Pretender.Create();
+            await Interlocked.Exchange(ref _last, pretender).WaitAsync();
             return pretender;
         }
     }
